64931add81baef3c5a11daf2427e976420d30dfc
Matty <nagamallimathi@gmail.com>
2025-01-03 21:22:21 +0530

    initial commit

---
+++ src/test/java/com/subscriptiontracker/SubscriptionTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.subscriptiontracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubscriptionTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
